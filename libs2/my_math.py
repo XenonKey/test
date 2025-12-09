2b3eef7a6f1f2fba7a49df6d372694aa510d7a48
giorgi <g.suleimanashvili7@gmail.com>
2025-12-09 15:47:06 +0400

    modified my_math.py

--- libs2/my_math.py
+++ libs2/my_math.py
@@ -2,16 +2,15 @@
 def say_hi():
     return 'Hello, User!'
 
+
 def multiply(a: int, b) -> int:
     return a * b
 
 
-<<<<<<< Updated upstream
 def mult_by_10(a) -> int:
     return a * 10
-=======
+
+
 def add_my_mail(mail):
     return f'This is my mail {mail}'
 
-print(add_my_mail('sds'))
->>>>>>> Stashed changes
